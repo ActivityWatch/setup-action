10a13610a22faf245adc7bd1238e071e42249cc0
Erik Bjäreholt <erik@bjareho.lt>
2023-11-21 12:25:53 +0100

    fix: updated REV for fetching aw-fakedata script

--- scripts/fakedata.sh
+++ scripts/fakedata.sh
@@ -1,5 +1,5 @@
-pip install git+https://github.com/ActivityWatch/aw-client.git
+pip install aw-client
 
-REV=63a357dbc7b8b832ec44cc55cafd6622ce909590
+REV=15bf361c2449522539657b901c4f9e77cae36074
 wget --no-verbose -O fakedata.py https://github.com/ActivityWatch/aw-fakedata/raw/$REV/aw_fakedata.py
 python3 fakedata.py
